75485e0ef4120508d29bd8bed306897c841e1d4e
EstuardFAVE <brayann.fave@gmail.com>
2021-04-05 19:20:05 -0300

    brayann farfan-agreguenuevo puerto y un archivo procfile

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const path = require('path');
+const puerto = process.env.PORT;
 app.use(express.static('public'));
 
 
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(puerto || 3000, function () {
     console.log('Example app listening on port '+ port);
 });
 
