0e3074c7d9aa5ac79ed468f61273ce4f03efadf5
EstuardFAVE <brayann.fave@gmail.com>
2021-04-17 07:09:52 -0300

    brayann farfan- trabaje con ejs y modifique las extensiones del html y los puse en su propia carpeta

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const path = require('path');
 const puerto = process.env.PORT;
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
 });
 
 app.listen(puerto || 3000, function () {
     console.log('Servidor corriendo en el puerto 3000');
 });
-
-app.get('/views/login.html', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
-});
-
-app.get('/views/register.html', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
-});
-
